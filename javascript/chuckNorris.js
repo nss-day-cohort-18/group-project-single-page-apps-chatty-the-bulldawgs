5613718ebac1d1a414229266c566d83bf4093bb2
short880 <shortjustin880@gmail.com>
2017-01-27 13:13:48 -0600

    This is the 1) iiFe requirement. It has not been tested but once we develop the connections we can test and debug. The bulk is there. after approval I'll merge to the master

--- javascript/chuckNorris.js
+++ javascript/chuckNorris.js
@@ -1,4 +1,64 @@
 'use strict'
 console.log("Is Chuck Here?: ")
 
-  var chuckJokes =  
+
+  var Chatty = (function(chuckNorris) {
+
+    var chuckJokes = [];
+
+
+    var dataRequest = new XMLHttpRequest();
+      dataRequest.addEventListener("load", dataRequestLoadComplete);
+      dataRequest.addEventListener("error", dataRequestError);
+
+
+
+/***** This function pulls the data from out Json file on the "load" eventListener. ****/
+    function dataRequestLoadComplete (event) {
+      console.log("Data Loaded");
+      var data = JSON.parse(event.target.responseText);
+      showData(data);
+    }
+
+    function dataRequestError(event) {
+      console.log("Failed load: ", event.target.responseText);
+
+    }
+
+
+/***** This function
+          - first assigns the output div
+          - for the variable jokes in data(object for JSON)
+          - chuckJokes creates an empty array
+          - pullJokes gets the value of the key[jokes]
+          - chuckJokes pushes all the information
+          - listDiv pushes the string to the HTML document
+******/
+
+    function showData(data){
+      var listDiv = document.getElementById("top");
+      for (i = 0; i < 5; i++) {
+        for (var jokes in data) {
+        var chuckJokes = '';
+        var pullJokes = data[jokes];
+            chuckJokes += "<article>";
+            chuckJokes += "<p>${pullJokes.joke} </p>";
+            chuckJokes += "<button type='button' value='delete' id='btn-delete'>"
+            chuckJokes += "</article>";
+        listDiv.innerHTML += pullJokes[i];
+    }
+  }
+}
+
+
+/****** This is accessing the JSON file
+        so it is avaiable when called by
+        the functions
+*******/
+
+dataRequest.open("GET", "chuck.json");
+dataRequest.send();
+
+return
+
+}); (Chatty || {})
