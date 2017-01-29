6ef1c8ea974796df5c37d5f3e6fde6721feb09ca
Talbot <talbotlawrence75@gmail.com>
2017-01-29 12:27:02 -0600

    Did more with taco tres

--- javascript/iiFeTres.js
+++ javascript/iiFeTres.js
@@ -1,45 +1,37 @@
 //verbs: accept, remove from DOM, remove from private array
 ///////////////////////////////////////////////////////////////////////////////////
-//remove element from the DOM
-
-// <div id="div1">
-// <p id="p1">This is a paragraph.</p>
-// <p id="p2">This is another paragraph.</p>
-// </div>
-
-// var parent = document.getElementById("div1");
-// var child = document.getElementById("p1");
-// parent.removeChild(child);
-
-///////////////////////////////////////////////////////////////////////////////////
-//the private array is userInput
-//userInput.splice(index, 1);
-
-
 
 'use strict';
 
 console.log("Hello from Tres");
 
 var Chatty = (function(originalChatty){
-console.log("userInput from Taco2 is: ", userInput);
+  console.log("userInput from Taco2 is: ", userInput);
+  //var userInput = [];
+  //var newMessage = document.getElementById("user");
 
-  originalChatty.removeMessage = function(input){
-          
-      // var whichIndex;
-      // jedi.forEach(function(item, index) {
-      //         if (item === who){
-      //                 whichIndex = index;
-      //         }   
-      // }); 
+  return {
 
-      input.splice(whichIndex, 1); 
-      console.log("Which index: ", whichIndex);
-  };  
+    originalChatty.removeMessageFromArray = function(input){
+        //get the buttonID--btn-delete2 for example
+        //possibly get the index number
+        //see splice below
+
+        input.splice(whichIndex, 1); 
+        console.log("Which index: ", whichIndex);
+        console.log("The new userInput array: ", userInput);
+    };  
+
+    originalChatty.removeMessageFromDom = function(input){
+      //get the buttonID--btn-delete2 for example
+      //possibly get the index number
+
+      var element = document.getElementById('user' + itemid ); 
+      element.parentNode.removeChild(element); 
+    }
+  }
 
 })(Chatty || {});
-
-
 
 //One IIFE should accept a message element id and then remove the correct element 
 //from the DOM. This IIFE should also remove the corresponding message from the 
