6da0057896cf23c29b9d0e58f2688990d262560f
Harry Epstein <harryeps@gmail.com>
2017-01-30 13:00:23 -0600

    changed name of array in main.js from array to messageArray

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,16 @@
-/*var inputBox = document.getElementById("userInput")
+var inputBox = document.getElementById("userInput")
 
 var Chatty = (function(){
 console.log("change stuff");
 `What is ${console.log("in template literal")}`
 
-}());*/
-var array = [];
+}());
+var messageArray = [];
 var userInput = document.getElementById('userInput');
-var last_element = array[array.length - 1];
+var last_element = messageArray[messageArray.length - 1];
 var btn_userInput = document.getElementById('btn-userInput');
 var userOutput = document.getElementById('userOutput');
-var outPut = "<div  id='newInput'>" + array.slice(-1)[0] + "<button type='button' class='btn btn-danger' id='btn-delete'>Chuck Chop This Joke</button>" + "</div>" + "<br>";
+var outPut = "<div  id='newInput'>" + messageArray.slice(-1)[0] + "<button type='button' class='btn btn-danger' id='btn-delete'>Chuck Chop This Joke</button>" + "</div>" + "<br>";
 
 document.getElementById('dark').addEventListener('click', function() {
     body.classList.toggle('new-element-class');
@@ -29,14 +29,14 @@ userInput.onkeyup = function () {
 
 /****** function (submitInput):
          1) executed on keyCode == '13' keyup (ABOVE)
-         2) pushes the userInput to the end of the array
+         2) pushes the userInput to the end of the messageArray
          3) output the string into the userOutput div.
 *******/
 
 function submitInput() {
   var newMessage = userInput.value;
-  array.push(newMessage);
-  console.log("array :", array);
-  document.querySelector('.userOutput').innerHTML += "<div  id='newInput'>" + array.slice(-1)[0] + "<button type='button' class='btn btn-danger' id='btn-delete'>Chuck Chop This Joke</button>" + "</div>" + "<br>";
-  console.log("output :", array)
+  messageArray.push(newMessage);
+  console.log("messageArray :", messageArray);
+  document.querySelector('.userOutput').innerHTML += "<div  id='newInput'>" + messageArray.slice(-1)[0] + "<button type='button' class='btn btn-danger' id='btn-delete'>Chuck Chop This Joke</button>" + "</div>" + "<br>";
+  console.log("output :", messageArray)
 }
