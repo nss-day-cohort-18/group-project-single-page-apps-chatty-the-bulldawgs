28d9d9e70c153d0bc2e4280c5b4fdae75d1ddb25
Harper Frankstone <Harper Frankstone>
2017-01-30 14:15:27 -0600

    commented a bunch in main.js, trying to figure out where and why it is breaking

--- javascript/main.js
+++ javascript/main.js
@@ -1,42 +1,61 @@
-var inputBox = document.getElementById("userInput")
+// var inputBox = document.getElementById("userInput")
+console.log("earth to major tom");
+
 
 var Chatty = (function(){
-console.log("change stuff");
-`What is ${console.log("in template literal")}`
+  console.log("change stuff");
+  `What is ${console.log("in template literal")}`;
 
-}());
-var messageArray = [];
-var userInput = document.getElementById('userInput');
-var last_element = messageArray[messageArray.length - 1];
-var btn_userInput = document.getElementById('btn-userInput');
-var userOutput = document.getElementById('userOutput');
-var outPut = "<div  id='newInput'>" + messageArray.slice(-1)[0] + "<button type='button' class='btn btn-danger' id='btn-delete'>Chuck Chop This Joke</button>" + "</div>" + "<br>";
+  return {
+      submitInput: function () {
+      var newMessage = userInput.value;
+      messageArray.push(newMessage);
+      console.log("messageArray :", messageArray);
+      document.querySelector('.userOutput').innerHTML += "<div  id='newInput'>" + messageArray.slice(-1)[0] + "<button type='button' class='btn btn-danger' id='btn-delete'>Chuck Chop This Joke</button>" + "</div>" + "<br>";
+      console.log("output :", messageArray)
+      }
 
-document.getElementById('dark').addEventListener('click', function() {
-    body.classList.toggle('new-element-class');
-document.getElementById('large').addEventListener('click', function() {
-    body.classList.toggle('new-larger-class');
-
-/****** document.userInput.onkeyup:
+      /****** document.userInput.onkeyup:
         1) if the user pushes enter
         2) the submitInput function is invoked.
-******/
-userInput.onkeyup = function () {
-    if (window.event.keyCode == '13') {
-        submitInput();
-    }
-}
+      ******/
+
+      // userInput: function () {
+      //   if (window.event.keyCode == '13'){
+      //   submitInput();
+      //   }
+      // };
+  }
+}());
+  console.log("Working here");
+//empty array to put messages in
+var messageArray = [];
+//the input field in the input div.input-group
+var userInput = document.getElementById('userInput');
+//the position of the last element of the array
+var last_element = messageArray[messageArray.length - 1];
+//this button doesn't exist
+var btn_userInput = document.getElementById('btn-userInput');
+//grabs the div which the user's input will show up in
+var userOutput = document.getElementById('userOutput');
+//creating a new div for the messages in messageArray
+var outPut = "<div  id='newInput'>" + messageArray.slice(-1)[0] + "<button type='button' class='btn btn-danger' id='btn-delete'>Chuck Chop This Joke</button>" + "</div>" + "<br>";
+//toggles the theme from light to dark
+document.getElementById('dark').addEventListener('click', function() {
+    div.classList.toggle('btn-darkTheme');//need to fix this class name since it doesn't exist 
+  })
+//toggles the font size from default size to bigger
+document.getElementById('large').addEventListener('click', function() {
+    div.classList.toggle('btn-largeText');//need to fix this class name since it doesn't exist
+  })
+
+
+
 
 /****** function (submitInput):
          1) executed on keyCode == '13' keyup (ABOVE)
          2) pushes the userInput to the end of the messageArray
-         3) output the string into the userOutput div. RANDOME CHANGE
+         3) output the string into the userOutput div. 
 *******/
 
-function submitInput() {
-  var newMessage = userInput.value;
-  messageArray.push(newMessage);
-  console.log("messageArray :", messageArray);
-  document.querySelector('.userOutput').innerHTML += "<div  id='newInput'>" + messageArray.slice(-1)[0] + "<button type='button' class='btn btn-danger' id='btn-delete'>Chuck Chop This Joke</button>" + "</div>" + "<br>";
-  console.log("output :", messageArray)
-}
+
