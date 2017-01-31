982e5dc5b7366aa1c76716197c654ad3ddafa35b
short880 <shortjustin880@gmail.com>
2017-01-31 13:08:23 -0600

    lots of changes, please double check all functions on branch. iiFe connected

--- javascript/main.js
+++ javascript/main.js
@@ -1,49 +1,27 @@
 console.log("main IIFE is working");
 
-var array = [];
 var userInput = document.getElementById('userInput');
-var last_element = array[array.length - 1];
 var btn_userInput = document.getElementById('btn-userInput');
 var userOutput = document.getElementById('userOutput');
-var outPut = "<div  id='newInput'>" + array.slice(-1)[0] + "<button type='button' class='btn btn-danger' id='btn-delete'>Chuck Chop This Joke</button>" + "</div>" + "<br>";
 
 
 /****** document.userInput.onkeyup:
         1) if the user pushes enter
         2) the submitInput function is invoked.
 ******/
-userInput.onkeyup = function () {
+userInput.addEventListener('keypress', function () {
     if (window.event.keyCode == '13') {
-        submitInput();
+        Chatty.submitInput(event);
     }
-}
+});
 
 
-/****** function (submitInput):
-         1) executed on keyCode == '13' keyup (ABOVE)
-         2) pushes the userInput to the end of the array
-         3) output the string into the userOutput div.
-*******/
 
-function submitInput() {
-  var newMessage = userInput.value;
-  if (newMessage.length < 1) {
-  alert("Chuck does not have time for your games...");
-  return;
-}
-  else {
-  array.push(newMessage);
-  console.log("array :", array);
-  userOutput.innerHTML += "<div id='newInput'>" + array.slice(-1)[0] + "<br>" + "<button type='button' class='btn btn-danger' id='btn-delete'>Chuck Chop This Joke</button>" + "</div>" + "<br>";
-  userInput.value = "";
-  console.log("output :", array)
-  }
-}
-
-
-/********This is the clear all fields button that is supposed to clear all the messages from the ouput divs*****/
-
-
+/********
+This is the clear all fields button
+that is supposed to clear all the messages
+from the ouput divs
+********/
 
 //function clearBox(jumbotron){
   function clearBox(){
